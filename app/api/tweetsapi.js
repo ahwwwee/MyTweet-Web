7f25d1029eb3845b3e851fc154d2d78f5a80e414
Ari <ahwwwee@gmail.com>
2016-12-21 16:28:23 +0000

    images woooooo!

--- app/api/tweetsapi.js
+++ app/api/tweetsapi.js
@@ -52,6 +52,7 @@ exports.create = {
     }
 
     tweet.save().then(tweet2 => {
+      console.log(tweet2._id);
 
       Tweet.find({ _id: tweet2._id }).populate('tweeter').populate('picture').then(newTweet => {
         console.log('payload: ' + data + ' end');
