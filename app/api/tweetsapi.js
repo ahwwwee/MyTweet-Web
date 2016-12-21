c7c74a2daf9d02c742169bbe76d1a178a934a478
Ari <ahwwwee@gmail.com>
2016-12-21 19:17:27 +0000

    images woooooo!

--- app/api/tweetsapi.js
+++ app/api/tweetsapi.js
@@ -45,7 +45,7 @@ exports.create = {
   handler: function (request, reply) {
     const data1 = request.payload.picture;
     const data = data1.data;
-    console.log(data);
+    console.log(data1);
     let tweet = new Tweet(request.payload);
     User.findOne({ _id:  request.params.id }).then(user => {
       tweet.tweeter = user;
